f3d89b4c689105a4d528666782c0e44906f430e0
Jeff Bowman <jeff@jbowman.com>
2011-02-13 04:02:32 -0800

    Extract further methods from render

--- jsscheduler.js
+++ jsscheduler.js
@@ -12,13 +12,21 @@
  * @constructor
  */
 Schedule = function(element, options) {
+	/** @type {Element} */
 	this.element_ = $(element).first();
+	/** @type {Object} */
 	this.options_ = $.extend({}, this.defaultOptions, options);
+	/** @type {Array.<String>} */
 	this.rowLabels_ = [];
+	/** @type {Array.<Column>} */
 	this.columns_ = [];
+	/** @type {Element} */
 	this.leftElement_ = null;
+	/** @type {Element} */
 	this.topElement_ = null;
+	/** @type {Element} */
 	this.gridElement_ = null;
+	/** @type {Element} */
 	this.topAxisLabelElement_ = null;
 };
 
@@ -38,7 +46,7 @@ Schedule.prototype.render = function() {
 	this.resize_();
 	this.createFrame_();
 	this.createRowHeaders_();
-	this.createBlocks_();
+	this.createColumns_();
 };
 
 /** @private */
@@ -100,95 +108,48 @@ Schedule.prototype.createRowHeaders_ = function() {
 };
 
 /** @private */
-Schedule.prototype.createBlocks_ = function() {
-	// add columns
+Schedule.prototype.createColumns_ = function() {
 	for (var colIndex = 0; colIndex < this.columns_.length; colIndex += 1) {
 		var column = this.columns_[colIndex];
-		column.createHeader(this, colIndex);
-		column.updateLayout(this);
-		
-		var colBlocks = column.blocks_;
-		for (var blockIndex in colBlocks) {
-			var block = colBlocks[blockIndex];
-			var blockElement = $(document.createElement("div"))
-				.addClass("schedule_gridcell")
-				.addClass(block.options_.cssClass)
-				.css({
-					backgroundColor: block.options_.borderColor,
-					color: block.options_.labelTextColor,
-					left: px(this.options_.columnWidth * colIndex + block.leftOffset_),
-					top: px(this.options_.rowHeight * block.row_),
-					width: px(this.options_.columnWidth - this.options_.cellMargin - block.leftOffset_ - block.rightOffset_),
-					height: px(block.height_ * this.options_.rowHeight - this.options_.cellMargin),
-					display: block.options_.visible ? "block" : "none"
-				});
-			if (block.height_ > 1) {
-				$(document.createElement("div"))
-					.addClass("schedule_cellcaption")
-					.text(block.label_)
-					.appendTo(blockElement);
-				$(document.createElement("div"))
-					.addClass("schedule_cellbody")
-					.css({
-						backgroundColor: block.options_.interiorColor,
-						color: block.options_.textColor
-					})
-					.text(block.main_)
-					.appendTo(blockElement);
-			} else {
-				$(document.createElement("div"))
-					.addClass("label")
-					.text(block.main_)
-					.appendTo(blockElement);
-				$(document.createElement("div"))
-					.addClass("main")
-					.appendTo(blockElement);
-			}
-			if (!block.options_.enabled) {
-				blockElement.addClass("disabled");
-			}
-			if (block.link_) {
-				blockElement.click({ block: block }, function(evt) {
-					location.href = evt.data.block.link_;
-				});
-			}
-			blockElement.mouseenter({ blockElement: blockElement }, function(evt) {
-				evt.data.blockElement.addClass("hover");
-			});
-			blockElement.mouseleave({ blockElement: blockElement }, function(evt) {
-				evt.data.blockElement.removeClass("hover");
-			});
-			
-			block.element_ = blockElement;
-			this.gridElement_.append(blockElement);
-		}
+		column.schedule_ = this;
+		column.index_ = colIndex;
+		column.createHeader_();
+		column.updateLayout_();
+		column.createBlocks_();
 	}
 };
 
 /**
  * Creates a Column object.
  * 
- * @param {any} id An ID used to map columns to locations.
  * @param {String} label Column title.
  * @param {String=} link URL to which the column header should link.
  * @param {Object} options A hash of options. 
  * @constructor
  */
-Column = function(id, label, link, options) {
-	this.id_ = id;
+Column = function(label, link, options) {
+	/** @type {String} */
 	this.label_ = label;
+	/** @type {String} */
 	this.link_ = link;
+	/** @type {Object} */
 	this.options_ = $.extend({}, this.defaultOptions, this.options);
+	/** @type {Array.<Block>} */
 	this.blocks_ = [];
+	/** @type {Element} */
 	this.element_ = null;
+	/** @type {Boolean} */
 	this.visible_ = true;
-	this.currentIndex_ = -1;
+	/** @type {Schedule} */
+	this.schedule_ = null;
+	/** @type {Number} */
+	this.index_ = -1;
 };
 
 Column.prototype.defaultOptions = {
 };
 
-Column.prototype.createHeader = function(schedule, colIndex) {
+Column.prototype.createHeader_ = function() {
 	if (this.element_) {
 		this.element_.parentElement.removeChild(this.element_);
 	}
@@ -196,8 +157,8 @@ Column.prototype.createHeader = function(schedule, colIndex) {
 	var columnHeader = $(document.createElement("div"))
 		.addClass("schedule_topheadercell")
 		.css({
-			left: px(colIndex * schedule.options_.columnWidth),
-			width: px(schedule.options_.columnWidth)
+			left: px(this.index_ * this.schedule_.options_.columnWidth),
+			width: px(this.schedule_.options_.columnWidth)
 		});
 	if (!this.link_) {
 		columnHeader.text(this.label_);
@@ -208,12 +169,12 @@ Column.prototype.createHeader = function(schedule, colIndex) {
 			.appendTo(columnHeader);
 	}
 	this.element_ = columnHeader;
-	schedule.topElement_.append(columnHeader);
+	this.schedule_.topElement_.append(columnHeader);
 };
 
-Column.prototype.updateLayout = function(schedule) {
+Column.prototype.updateLayout_ = function() {
 	var rowData = [];
-	for (var i = 0, len = schedule.rowLabels_.length; i < len; i += 1) {
+	for (var i = 0, len = this.schedule_.rowLabels_.length; i < len; i += 1) {
 		rowData[i] = [];
 	}
 	for (var blockIndex in this.blocks_) {
@@ -227,7 +188,7 @@ Column.prototype.updateLayout = function(schedule) {
 	
 	var maxCrowding = 0;
 	
-	for (var i = 0; i < schedule.rowLabels_.length; i += 1) {
+	for (var i = 0; i < this.schedule_.rowLabels_.length; i += 1) {
 		for (var rowBlockIndex in rowData[i]) {
 			var rowBlock = rowData[i][rowBlockIndex];
 			if (rowBlock.subColumnId_ == -1) {
@@ -253,8 +214,17 @@ Column.prototype.updateLayout = function(schedule) {
 	
 	for (var blockIndex in this.blocks_) {
 		var block = this.blocks_[blockIndex];
-		block.leftOffset_ = block.subColumnId_ * schedule.options_.adjacentStep;
-		block.rightOffset_ = (maxCrowding - block.subColumnId_) * schedule.options_.adjacentStep;
+		block.leftOffset_ = block.subColumnId_ * this.schedule_.options_.adjacentStep;
+		block.rightOffset_ = (maxCrowding - block.subColumnId_) * this.schedule_.options_.adjacentStep;
+	}
+};
+
+Column.prototype.createBlocks_ = function() {
+	for (var blockIndex in this.blocks_) {
+		var block = this.blocks_[blockIndex];
+		block.schedule_ = this.schedule_;
+		block.column_ = this;
+		block.createCell_();
 	}
 };
 
@@ -267,17 +237,31 @@ Column.prototype.updateLayout = function(schedule) {
  * @param {Object} options A hash of options.
  * @constructor
  */
-Block = function(label, main, row, height, link, options) { 
+Block = function(label, main, row, height, link, options) {
+	/** @type {String} */
 	this.label_ = label;
+	/** @type {String} */
 	this.main_ = main;
+	/** @type {Number} */
 	this.row_ = row;
+	/** @type {Number} */
 	this.height_ = height;
+	/** @type {String} */
 	this.link_ = link;
+	/** @type {Object} */
 	this.options_ = $.extend({}, this.defaultOptions, options);
 	
+	/** @type {Element} */
 	this.element_ = null;
+	/** @type {Schedule} */
+	this.schedule_ = null;
+	/** @type {Column} */
+	this.column_ = null;
+	/** @type {Number} */
 	this.leftOffset_ = 0;
+	/** @type {Number} */
 	this.rightOffset_ = 0;
+	/** @type {Number} */
 	this.subColumnId_ = 0;
 };
 
@@ -289,6 +273,63 @@ Block.prototype.defaultOptions = {
 	enabled: true,
 	visible: true,
 	cssClass: ''
+};
+
+Block.prototype.createCell_ = function() {
+	if (this.element_) {
+		this.element_.parentElement.removeChild(this.element_);
+	}
+	var blockElement = $(document.createElement("div"))
+		.addClass("schedule_gridcell")
+		.addClass(this.options_.cssClass)
+		.css({
+			backgroundColor: this.options_.borderColor,
+			color: this.options_.labelTextColor,
+			left: px(this.schedule_.options_.columnWidth * this.column_.index_ + this.leftOffset_),
+			top: px(schedule.options_.rowHeight * this.row_),
+			width: px(schedule.options_.columnWidth - schedule.options_.cellMargin - this.leftOffset_ - this.rightOffset_),
+			height: px(this.height_ * schedule.options_.rowHeight - schedule.options_.cellMargin),
+			display: this.options_.visible ? "block" : "none"
+		});
+	if (this.height_ > 1) {
+		$(document.createElement("div"))
+			.addClass("schedule_cellcaption")
+			.text(this.label_)
+			.appendTo(blockElement);
+		$(document.createElement("div"))
+			.addClass("schedule_cellbody")
+			.css({
+				backgroundColor: this.options_.interiorColor,
+				color: this.options_.textColor
+			})
+			.text(this.main_)
+			.appendTo(blockElement);
+	} else {
+		$(document.createElement("div"))
+			.addClass("label")
+			.text(this.main_)
+			.appendTo(blockElement);
+		$(document.createElement("div"))
+			.addClass("main")
+			.appendTo(blockElement);
+	}
+	if (!this.options_.enabled) {
+		blockElement.addClass("disabled");
+	}
+	if (this.link_) {
+		blockElement.click({ block: this }, function(evt) {
+			location.href = evt.data.block.link_;
+		});
+	}
+	blockElement.mouseenter({ blockElement: blockElement }, function(evt) {
+		evt.data.blockElement.addClass("hover");
+	});
+	blockElement.mouseleave({ blockElement: blockElement }, function(evt) {
+		evt.data.blockElement.removeClass("hover");
+	});
+	
+	this.element_ = blockElement;
+	this.schedule_.gridElement_.append(blockElement);
 };
 
 function px(value) {
